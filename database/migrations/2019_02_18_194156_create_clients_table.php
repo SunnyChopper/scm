d0032a6729e10da74f0824c7338963f7d4e240ec
Singh, Sunny <isingh3@ilstu.edu>
2019-02-18 13:48:18 -0600

    Created database migrations for client dashboard

---
+++ database/migrations/2019_02_18_194156_create_clients_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name', 128);
+            $table->string('last_name', 128)->nullable();
+            $table->string('email', 255);
+            $table->string('password', 255);
+            $table->string('company_name', 128);
+            $table->string('logo_url', 255)->nullable();
+            $table->integer('is_active')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
