34bcd88d82804caf07d0cbd11b9120ba828d499e
Singh, Sunny <isingh3@ilstu.edu>
2019-02-28 14:27:25 -0600

    Added some functions for clients

--- app/Http/Controllers/ClientsController.php
+++ app/Http/Controllers/ClientsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Client;
 
 class ClientsController extends Controller
 {
@@ -23,4 +24,33 @@ class ClientsController extends Controller
 
     	return view('clients.dashboard')->with('page_title', $page_title);
     }
+
+    public function create_client(Request $data) {
+        // Create the client
+        $client = new Client;
+        $client->first_name = $data->first_name;
+        $client->last_name = $data->last_name;
+        $client->email = $data->email;
+        $client->password = $data->password;
+        $client->company_name = $data->company_name;
+
+        if (isset($data->logo_url)) {
+            $client->logo_url = $data->logo_url;
+        }
+    }
+
+    public function update_client(Request $data) {
+        // Update the client
+        $client = Client::where('id', $data->client_id)->get();
+        $client->first_name = $data->first_name;
+        $client->last_name = $data->last_name;
+        $client->email = $data->email;
+        $client->password = $data->password;
+        $client->company_name = $data->company_name;
+
+        if (isset($data->logo_url)) {
+            $client->logo_url = $data->logo_url;
+        }
+    }
+
 }
---
+++ app/Revenue.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Revenue extends Model
+{
+    protected $table = "revenues";
+    public $primaryKey = "id";
+}
