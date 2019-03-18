6cd1e09539ca27df84bf0dd15f7cea3770f5ed3c
Sunny Singh <ishy.singh@gmail.com>
2019-03-18 02:50:37 -0500

    Polishing up site

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -7,7 +7,7 @@
 						<h3>Vision Statement</h3>
 					</div>
 					<p>Our vision is to help dedicated influencers grow their brand and create a solid business. We achieve this by developing infrastructure and assets to boost engagement and create more touch points with the brand. All of this gives way to monetization.</p>
-					<p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+					<p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->Copyright {{ config('app.name') }} &copy; <script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
 				</aside>
 			</div>
 			<div class="col-lg-5 col-sm-6">
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -15,7 +15,7 @@
 					<div class="collapse navbar-collapse offset" id="navbarSupportedContent">
 						<ul class="nav navbar-nav menu_nav justify-content-center">
 							<li class="nav-item"><a class="nav-link" href="/">Home</a></li>
-							<li class="nav-item"><a class="nav-link" href="">Tips & Advice</a></li>
+							{{-- <li class="nav-item"><a class="nav-link" href="">Tips & Advice</a></li> --}}
 							<li class="nav-item"><a class="nav-link" href="/pricing">Pricing</a></li>
 							<li class="nav-item submenu dropdown">
 								<a href="/services" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Services</a>
