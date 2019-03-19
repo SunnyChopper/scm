0e9955ea6d9ad642ee334a3e01e0101a5657faab
Sunny Singh <ishy.singh@gmail.com>
2019-03-18 21:58:43 -0500

    Added styling to logs table

--- resources/views/admin/logs/view.blade.php
+++ resources/views/admin/logs/view.blade.php
@@ -12,18 +12,18 @@
 		@if(count($logs) > 0)
 			<div class="row">
 				<div class="col-12 mb-16">
-					<a href="/admin/logs/new">New Log</a>
+					<a href="/admin/logs/new" class="primary_btn pr-4 pl-4">New Log</a>
 				</div>
 
 				<div class="col-12">
 					<div style="overflow: auto;">
-						<table>
+						<table class="table table-striped">
 							<thead>
 								<tr>
-									<td>Company Name</td>
-									<td>Title</td>
-									<td>Description</td>
-									<td></td>
+									<th>Company Name</th>
+									<th>Title</th>
+									<th>Description</th>
+									<th></th>
 								</tr>
 							</thead>
 							<tbody>
