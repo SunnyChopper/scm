9c564743ece689ef35ed22adbb74bfb58346882a
Sunny Singh <ishy.singh@gmail.com>
2019-08-31 14:50:52 -0500

    Added Facebook Pixel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -74,6 +74,22 @@
 		<link rel="stylesheet" type="text/css" href="{{ URL::asset('css/app.css') }}">
 		<link rel="stylesheet" type="text/css" href="{{ URL::asset('css/layouts.css') }}">
 
+		<!-- Facebook Pixel -->
+		<script>
+			!function(f,b,e,v,n,t,s)
+			{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+			n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+			if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+			n.queue=[];t=b.createElement(e);t.async=!0;
+			t.src=v;s=b.getElementsByTagName(e)[0];
+			s.parentNode.insertBefore(t,s)}(window, document,'script',
+			'https://connect.facebook.net/en_US/fbevents.js');
+			fbq('init', '1225441154310154');
+			fbq('track', 'PageView');
+		</script>
+		<noscript>
+			<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1225441154310154&ev=PageView&noscript=1"/>
+		</noscript>
 	</head>
 	<body>
 		@include('layouts.navbar')
